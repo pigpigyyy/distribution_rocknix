8565487462f4a543baca21cbbb40bb9ab896125b
Li Jin <dragon-fly@qq.com>
2026-08-26 13:21:31 +0800

    evtest: replace broken repo.or.cz snapshot

--- packages/addons/addon-depends/system-tools-depends/evtest/package.mk
+++ packages/addons/addon-depends/system-tools-depends/evtest/package.mk
@@ -3,10 +3,10 @@
 
 PKG_NAME="evtest"
 PKG_VERSION="1.35"
-PKG_SHA256="a224abeb783561193660999b0c986216d691cb271b5ec3af88c6bb37b55ff51c"
+PKG_SHA256="a303eaf9dc5e8821806a4650fa100c5221bc89bd82dc4c725161f7901b453fdd"
 PKG_LICENSE="GPL"
 PKG_SITE="http://cgit.freedesktop.org/evtest/"
-PKG_URL="https://repo.or.cz/evtest.git/snapshot/evtest-${PKG_VERSION}.tar.gz"
+PKG_URL="https://deb.debian.org/debian/pool/main/e/evtest/evtest_${PKG_VERSION}.orig.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libxml2"
 PKG_LONGDESC="A simple tool for input event debugging."
 PKG_TOOLCHAIN="autotools"
